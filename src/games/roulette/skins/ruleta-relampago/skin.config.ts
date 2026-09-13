9dd721c78107cbdc5201301ac8efa5c39c05680f
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-09-13 19:29:43 -0300

    Add sanitized demo project: src/games/roulette/skins/ruleta-relampago/skin.config.ts

---
+++ src/games/roulette/skins/ruleta-relampago/skin.config.ts
@@ -0,0 +1,14 @@
+import { RouletteSkin } from '../../../../types';
+
+export const ruletaRelampagoSkin: RouletteSkin = {
+  id: 'ruleta-relampago',
+  name: 'Ruleta Relámpago Criolla',
+  title: 'Ruleta Relámpago Criolla ⚡',
+  clothColor: '#07101e',
+  feltTexture: 'high_contrast_dark_felt',
+  trimColor: '#00e5ff',
+  accentColor: '#ffd700',
+  isLightning: true,
+  maxMultiplier: 500,
+  specialSound: 'thunder_strike_electrifying',
+};
