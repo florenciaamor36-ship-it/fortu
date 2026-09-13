8fa85743ababfa8376cf02d1feeaa127485dc2b2
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-09-13 19:29:40 -0300

    Add sanitized demo project: src/games/bingo/skins/bingo-criollo/skin.config.ts

---
+++ src/games/bingo/skins/bingo-criollo/skin.config.ts
@@ -0,0 +1,12 @@
+import { BingoSkin } from '../../../../types';
+
+export const bingoCriolloSkin: BingoSkin = {
+  id: 'bingo-criollo',
+  name: 'Gran Bingo Peña Criolla',
+  title: 'Gran Bingo Criollo 75 Bolillas',
+  cardBg: '#0f1a30',
+  ballTheme: 'gold_metallic_spheres',
+  drumColor: '#c5a059',
+  accentColor: '#ffd700',
+  ballsCount: 75,
+};
