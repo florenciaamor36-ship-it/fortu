c72a6a8cae00fed4d037e0c5f4d5135679c7f810
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-09-13 19:29:41 -0300

    Add sanitized demo project: src/games/bingo/skins/bingo-patagonia/skin.config.ts

---
+++ src/games/bingo/skins/bingo-patagonia/skin.config.ts
@@ -0,0 +1,12 @@
+import { BingoSkin } from '../../../../types';
+
+export const bingoPatagoniaSkin: BingoSkin = {
+  id: 'bingo-patagonia',
+  name: 'Bingo Glaciar Patagonia',
+  title: 'Bingo Glaciar Austral',
+  cardBg: '#081729',
+  ballTheme: 'cyan_ice_crystal_spheres',
+  drumColor: '#00e5ff',
+  accentColor: '#80d8ff',
+  ballsCount: 75,
+};
