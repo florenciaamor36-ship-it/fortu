f0646d49064e2ec1f9bb14091c97bc3c4272fc20
florenciaamor36-ship-it <florenciaamor36@gmail.com>
2026-09-13 19:29:42 -0300

    Add sanitized demo project: src/games/roulette/skins/ruleta-portena-vip/skin.config.ts

---
+++ src/games/roulette/skins/ruleta-portena-vip/skin.config.ts
@@ -0,0 +1,14 @@
+import { RouletteSkin } from '../../../../types';
+
+export const ruletaPortenaVipSkin: RouletteSkin = {
+  id: 'ruleta-portena-vip',
+  name: 'Ruleta Porteña VIP Puerto Madero',
+  title: 'Ruleta Europea Clásica VIP',
+  clothColor: '#0b1b36',
+  feltTexture: 'deep_navy_luxury_cloth',
+  trimColor: '#c5a059',
+  accentColor: '#fae5b8',
+  isLightning: false,
+  maxMultiplier: 36,
+  specialSound: 'ivory_ball_wood_wheel_chime',
+};
